09d1b8e5d6bf2c5f8f6d3ae777c8a4e30d3214e6
Diego Rebollo <diego@drebollo.dev>
2022-10-14 17:31:41 +0200

    minor fixes

--- control.py
+++ control.py
@@ -1,53 +1,62 @@
 from machine import ADC, Pin
 from date_to_file import save_date, read_file
-import utime 
+import utime
 
-"""
-SENSOR = ADC(26)
-RELAY = Pin(7, Pin.OUT)
-SECONDS_PUMPING = 3 
-SECONDS_READOUT = 3
-THRESHOLD = 22600
-RELAY_STATUS = None
-"""
-vars_file = read_file('test.json')
 
-SENSOR = ADC(vars_file['sensor_pin'])
-RELAY = Pin(vars_file['relay_pin'], Pin.OUT)
-SECONDS_PUMPING = vars_file['seconds_pumping'] 
-SECONDS_READOUT = vars_file['seconds_readout'] 
-THRESHOLD = vars_file['threshold'] 
+SENSOR = None
+RELAY = None
+SECONDS_PUMPING = None
+SECONDS_READOUT = None
+THRESHOLD = None
 RELAY_STATUS = None
+VAR_FILE = 'variables.json'
+
+
+def update_var():
+    
+    global SENSOR, RELAY, SECONDS_PUMPING, SECONDS_READOUT, THRESHOLD
+    vars_file = read_file(VAR_FILE)
+
+    SENSOR = ADC(vars_file['sensor_pin'])
+    RELAY = Pin(vars_file['relay_pin'], Pin.OUT)
+    SECONDS_PUMPING = vars_file['seconds_pumping']
+    SECONDS_READOUT = vars_file['seconds_readout']
+    THRESHOLD = vars_file['threshold']
+
 
 def water_sensor_readout():
-    
+
     sensor_read = SENSOR.read_u16()
     return sensor_read
 
 
 def relay_off():
-    
+
     global RELAY_STATUS
     RELAY.value(0)
     RELAY_STATUS = False
     return "Relay Off"
-    
+
 
 def relay_on():
-    
-    global RELAY_STATUS    
+
+    global RELAY_STATUS
     RELAY.value(1)
     RELAY_STATUS = True
     save_date()
     return "Relay On"
 
 
-def main():      
-    while True:
-        print(water_sensor_readout())
-        relay_off()        
-        if water_sensor_readout() < THRESHOLD:            
+def main():    
+    
+    update_var()
+    print('main func working')
+    while True:        
+        relay_off()
+        if water_sensor_readout() < THRESHOLD:
             relay_on()
-            utime.sleep(SECONDS_PUMPING)            
+            utime.sleep(SECONDS_PUMPING)
         else:
             utime.sleep(SECONDS_READOUT)
+            
+            
--- testing.py
+++ testing.py
@@ -15,18 +15,18 @@ _thread.start_new_thread(control.main, ())
 # Connect to WIFI
 try:
     mm_wlan.connect_to_network(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
-except:    
+except:
     utime.sleep(180)
     mm_wlan.connect_to_network(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
-    
-# Set Time    
+
+# Set Time
 try:
     ntptime.settime()
 except:
     utime.sleep(10)
     ntptime.settime()
 
-# Web Server  
+# Web Server
 app = Microdot()
 set_session_secret_key(secrets.SECRET_KEY)
 Response.default_content_type = 'text/html'
@@ -35,13 +35,14 @@ Response.default_content_type = 'text/html'
 @app.route('', methods=['GET', 'POST'])
 @with_session
 def index(request, session):
-    
+
     session_status = session.get('status')
     water_sensor_readout = control.water_sensor_readout()
     last_run_date = get_last_record()
     realy_status = control.RELAY_STATUS
     threshold = control.THRESHOLD
-    vars_file = read_file('test.json')    
+    vars_file = read_file('variables.json')
+    
     if session_status is not 'authorized':
         return render_template('home.html', session_status=session_status, water_sensor_readout=water_sensor_readout, last_run_date=last_run_date, threshold=threshold, realy_status=realy_status)
     else:
@@ -49,22 +50,25 @@ def index(request, session):
             var_name = request.form['var_name']
             new_value = request.form['new_value']
             vars_file[var_name] = int(new_value)
-            save_file('test.json', vars_file)
+            save_file('variables.json', vars_file)
             print(vars_file)
+            control.update_var()
+            return redirect('/')
         else:
-            return render_template('manage.html', vars_file=vars_file, session_status=session_status, water_sensor_readout=water_sensor_readout, last_run_date=last_run_date, threshold=threshold, realy_status=realy_status)       
+            return render_template('manage.html', vars_file=vars_file, session_status=session_status, water_sensor_readout=water_sensor_readout, last_run_date=last_run_date, threshold=threshold, realy_status=realy_status)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 @with_session
-def login(request, session):    
-    session_status = session.get('status')    
-    if request.method == 'POST':        
+def login(request, session):
+    session_status = session.get('status')
+    if request.method == 'POST':
         if request.form['user'] == secrets.USER and request.form['pwd'] == secrets.PASSWORD:
             update_session(request, {'status': 'authorized'})
             return redirect('/')
         else:
             return render_template('login.html', session_status=session_status, msg="Incorrect username or password.")
-    elif session_status is not 'authorized':            
+    elif session_status is not 'authorized':
         return render_template('login.html', session_status=session_status, msg="")
     else:
         return redirect('/')
@@ -91,10 +95,17 @@ def relay_off(request, session):
         control.relay_off()
         return redirect('/')
 
+
 @app.route('/logout', methods=['GET'])
 def logout(request):
     delete_session(request)
     return redirect('/')
 
-app.run(port=80, debug=True)
+@app.get('/history')
+def index(request):
+    date_file = read_file('date_history.json')
+    return date_file
+
+
+app.run(port=80)
 
